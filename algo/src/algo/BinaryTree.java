d91cdf07e346a1c25291ee54abfeecdeef639b40
JessicaBarai <mejlbarai@gmail.com>
2021-04-02 15:58:33 +0200

    try 1

---
+++ algo/src/algo/BinaryTree.java
@@ -0,0 +1,96 @@
+package algo;
+
+public class BinaryTree {
+
+	private Node<Integer> root;
+	private int count=0;
+	// first we need to find right place where we want to add a new Node
+	// starting from root node
+	private Node<Integer> addRecursive(Node<Integer> current, int value) {
+		if (current == null) {
+			return new Node<Integer>(value);
+
+		}
+
+		if (value < current.value) {
+			current.left = addRecursive(current.left, value);
+
+		} else if (value > current.value) {
+			current.right = addRecursive(current.right, value);
+
+		} else
+			return current; // value exist
+		return current;
+	}
+
+	// this will add all nodes starting from root node
+
+	public void add(int value) {
+		addRecursive(root, value);
+		// System.out.print("root" + String.valueOf(root.value));
+	}
+
+	public void traverseInOrder(Node<Integer> node) {
+		if (node != null) {
+			traverseInOrder(node.left);
+			System.out.print("" + node.value);
+			traverseInOrder(node.right);
+		}
+
+	}
+
+	/*public static void getLevel(BinaryTree tree, int level) {
+		if(level==0) {
+			System.out.println(String.valueOf(tree.root.value));
+			return;
+			
+		}while(count<level) {
+			
+			
+		}
+		
+
+	}*/
+
+	public void traverseLevel(Node<Integer> node) {
+		count++;
+		if (node == root) {
+			System.out.println("" + node.value);
+			count=0;
+			
+		}
+		if (node.left != null ) {
+			System.out.print("" + node.left.value);
+			
+		}
+		if (node.right != null) {
+			System.out.print("" + node.right.value);
+
+		}
+
+		if (node.left != null) {
+			traverseLevel(node.left);
+		}
+		if (node.right != null) {
+			traverseLevel(node.right);
+		}
+		System.out.println("level"+String.valueOf(count));
+	}
+
+	public static void main(String[] args) {
+		BinaryTree bt = new BinaryTree();
+		bt.root = new Node<Integer>(6);
+
+		bt.add(4);
+		bt.add(8);
+		bt.add(3);
+		bt.add(5);
+		bt.add(7);
+		bt.add(9);
+
+		// bt.traverseInOrder(bt.root);
+
+		bt.traverseLevel(bt.root);
+	}
+
+}
--- algo/src/algo/Node.java
+++ algo/src/algo/Node.java
@@ -1,5 +1,17 @@
 package algo;
 
-public class Node {
+public class Node<T> {
+	 T value;
+	 Node<T> left;
+	 Node<T> right;
+	 
+	 
+	 Node(T value){
+		 this.value=value;
+		 right=null;
+		 left=null;
+		 
+	 }
+	 
 
 }
