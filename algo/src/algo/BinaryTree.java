54aa2a2e52ad9dedbd8fe3779a096927c1d414b7
JessicaBarai <mejlbarai@gmail.com>
2021-04-04 10:20:53 +0200

    printLevels impl.

--- algo/src/algo/BinaryTree.java
+++ algo/src/algo/BinaryTree.java
@@ -3,7 +3,7 @@ package algo;
 public class BinaryTree {
 
 	private Node<Integer> root;
-	private int count=0;
+	private static int count=0;
 	// first we need to find right place where we want to add a new Node
 	// starting from root node
 	private Node<Integer> addRecursive(Node<Integer> current, int value) {
@@ -30,7 +30,7 @@ public class BinaryTree {
 		// System.out.print("root" + String.valueOf(root.value));
 	}
 
-	public void traverseInOrder(Node<Integer> node) {
+	public static void traverseInOrder(Node<Integer> node) {
 		if (node != null) {
 			traverseInOrder(node.left);
 			System.out.print("" + node.value);
@@ -38,19 +38,52 @@ public class BinaryTree {
 		}
 
 	}
-
-	/*public static void getLevel(BinaryTree tree, int level) {
-		if(level==0) {
-			System.out.println(String.valueOf(tree.root.value));
-			return;
-			
-		}while(count<level) {
-			
-			
+	
+	
+		public static void printGivenLevel(Node<Integer> root, int level)
+		{
+		    if (root == null)
+		        return;
+		    if (level == 1)
+		        System.out.print(root.value + ",");
+		    else if (level > 1)
+		    {
+		        printGivenLevel(root.left, level-1);
+		        printGivenLevel(root.right, level-1);
+		    }
 		}
 		
+	
 
-	}*/
+	/*public static void getLevel(BinaryTree tree, int level) {
+		
+		
+		
+		if( tree.root!=null) {
+			
+			tree.root = tree.root.left;
+			
+			count++;
+			if(count==level) {
+				//now we need to print out this node and this node's parents right child
+			printAtGivenLevel(tree.root);
+				return;
+			}
+			if(tree.root==null) {
+				System.out.println("no such level");
+				return;
+				
+			}
+			getLevel(tree,level);
+			
+		}*/
+		
+		
+	
+			
+		
+
+	
 
 	public void traverseLevel(Node<Integer> node) {
 		count++;
@@ -89,8 +122,10 @@ public class BinaryTree {
 		bt.add(9);
 
 		// bt.traverseInOrder(bt.root);
-
-		bt.traverseLevel(bt.root);
+		printGivenLevel(bt.root,3);
+		//bt.traverseLevel(bt.root);
+		
+		//getLevel(bt,3);
 	}
 
 }
